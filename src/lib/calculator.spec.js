8812436a24bc55899f2a36c4ca4aeff647545c53
Christian Gonçalves <christiangoncalvesr@gmail.com>
2022-01-04 22:58:37 -0300

    style: run prettier on files

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -1,23 +1,23 @@
 const { sum } = require('./calculator')
 
 it('should sum 2 to 2 and the result must be 4', () => {
-    expect(sum(2, 2)).toBe(4)
+  expect(sum(2, 2)).toBe(4)
 })
 
 it('should be sum 2 to 2 even if one of them is a string and the result must be 4', () => {
-    expect(sum('2', '2')).toBe(4)
+  expect(sum('2', '2')).toBe(4)
 })
 
 it('should throw an error if it has no parameters or if one of the parameters supplied to the method cannot be summed', () => {
-    expect(() => {
-        sum('', '2')
-    }).toThrowError()
+  expect(() => {
+    sum('', '2')
+  }).toThrowError()
 
-    expect(() => {
-        sum([2, 2])
-    }).toThrowError()
+  expect(() => {
+    sum([2, 2])
+  }).toThrowError()
 
-    expect(() => {
-        sum()
-    }).toThrowError()
+  expect(() => {
+    sum()
+  }).toThrowError()
 })
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,12 +1,12 @@
 const { queryString } = require('./queryString')
 
 describe('Object to query string', () => {
-    it('should create a valid query string when the object in the parameter is valid', () => {
-        const obj = {
-            name: 'christian',
-            profession: 'developer'
-        }
+  it('should create a valid query string when the object in the parameter is valid', () => {
+    const obj = {
+      name: 'christian',
+      profession: 'developer'
+    }
 
-        expect(queryString(obj)).toBe('name=christian&profession=developer')
-    })
+    expect(queryString(obj)).toBe('name=christian&profession=developer')
+  })
 })
