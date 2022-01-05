172c37063c9f82eb9bb195fb96d38ce9482ced78
Christian Gonçalves <christiangoncalvesr@gmail.com>
2022-01-04 23:54:25 -0300

    test: add new tests to queryString method

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,6 +1,16 @@
+const keyAndValueToString = objectEntry => {
+  const [_key, value] = objectEntry
+
+  if (!Array.isArray(value) && typeof value === 'object') {
+    throw new Error('Please check your input, only primitive values are allowed')
+  }
+
+  return objectEntry.join('=')
+}
+
 module.exports.queryString = obj => {
   const entries = Object.entries(obj)
-  const mappedQueries = entries.map(entry => entry.join('='))
+  const mappedQueries = entries.map(keyAndValueToString)
   const queryString = mappedQueries.join('&')
 
   return queryString
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,6 +1,6 @@
 const { queryString } = require('./queryString')
 
-describe('Object to query string', () => {
+describe('query string', () => {
   it('should create a valid query string when the object in the parameter is valid', () => {
     const obj = {
       name: 'christian',
@@ -8,5 +8,28 @@ describe('Object to query string', () => {
     }
 
     expect(queryString(obj)).toBe('name=christian&profession=developer')
+  })
+
+  it('should create a valid query string when an array is the value of param', () => {
+    const obj = {
+      name: 'christian',
+      abilities: ['JS', 'TS']
+    }
+
+    expect(queryString(obj)).toBe('name=christian&abilities=JS,TS')
+  })
+
+  it('should throw an error when an object is passed in the value of param', () => {
+    const obj = {
+      name: 'christian',
+      abilities: {
+        first: 'JS',
+        second: 'TS'
+      }
+    }
+
+    expect(() => {
+      queryString(obj)
+    }).toThrowError()
   })
 })
